2e4204b91b2e2230222d528a76c0f962cdd23742
Jonas Bernoulli <jonas@bernoul.li>
2024-01-06 01:34:41 +0100

    Fix pasto in commentary and readme

--- notmuch-transient.el
+++ notmuch-transient.el
@@ -30,7 +30,7 @@
 
 ;;; Commentary:
 
-;; This package command provides dispatcher for existing Notmuch
+;; This package provides a command dispatcher for existing Notmuch
 ;; commands, as well as some new commands for dealing with tags.
 
 ;; Everything is still incomplete and very much subject to change.
