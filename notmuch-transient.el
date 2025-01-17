4902879d93402e140a048def88f8fec8cf45d9cb
Jonas Bernoulli <jonas@bernoul.li>
2025-01-17 13:41:36 +0100

    Release version 1.1.0

--- notmuch-transient.el
+++ notmuch-transient.el
@@ -7,7 +7,7 @@
 ;; Homepage: https://github.com/tarsius/notmuch-transient
 ;; Keywords: mail
 
-;; Package-Version: 1.0.1
+;; Package-Version: 1.1.0
 ;; Package-Requires: (
 ;;     (emacs "29.1")
 ;;     (compat "30.0.2.0")
